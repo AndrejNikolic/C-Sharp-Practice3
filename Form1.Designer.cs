bdf359eac30346899868e4a962b6c19bfb781e79
Andrej Nikolić <existence.of.utopia@gmail.com>
2020-02-07 18:49:58 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace DomaciZadatak12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startTime = new System.Windows.Forms.DateTimePicker();
+            this.endTime = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startTime
+            // 
+            this.startTime.CustomFormat = "HH:mm";
+            this.startTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.startTime.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.startTime.Location = new System.Drawing.Point(51, 41);
+            this.startTime.Name = "startTime";
+            this.startTime.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.startTime.ShowUpDown = true;
+            this.startTime.Size = new System.Drawing.Size(70, 20);
+            this.startTime.TabIndex = 0;
+            this.startTime.Value = new System.DateTime(2019, 12, 30, 20, 0, 0, 0);
+            // 
+            // endTime
+            // 
+            this.endTime.CustomFormat = "HH:mm";
+            this.endTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.endTime.Location = new System.Drawing.Point(156, 41);
+            this.endTime.Name = "endTime";
+            this.endTime.ShowUpDown = true;
+            this.endTime.Size = new System.Drawing.Size(73, 20);
+            this.endTime.TabIndex = 1;
+            this.endTime.Value = new System.DateTime(2019, 12, 30, 21, 15, 0, 0);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Leave time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Arrival time";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Travel time";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(51, 123);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 13);
+            this.result.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 188);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.endTime);
+            this.Controls.Add(this.startTime);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Domaći Zadatak 12";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker startTime;
+        private System.Windows.Forms.DateTimePicker endTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DomaciZadatak12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int start = startTime.Value.Hour * 60 + startTime.Value.Minute;
+            int end = endTime.Value.Hour * 60 + endTime.Value.Minute;
+
+            int resultMins, resultHours;
+
+            if (start == end) {
+                MessageBox.Show("Leave and arrival time can't be the same", "Warning!", MessageBoxButtons.OK);
+            }
+            else if (start < end)
+            {
+                resultMins = (end - start) % 60;
+                resultHours = ((end - start) - resultMins) / 60;
+                String strHour = "";
+                if (resultHours < 2)
+                {
+                    strHour = " hour";
+                }else
+                {
+                    strHour = " hours";
+                }
+
+                result.Text = "Plane will land in: " + resultHours.ToString() + strHour + " and " + resultMins.ToString() + " minutes";
+            }
+            else {
+                resultMins = 60 + (end - start) % 60;
+                resultHours = 24 + ((end - start) - resultMins) / 60;
+
+                result.Text = "Plane will land in: " + resultHours.ToString() + " hours and " + resultMins.ToString() + " minutes";
+            }
+            
+        }
+    }
+}
